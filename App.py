7c22016fcb8115c56285e6b70944c8d748ee971e
SamikshaChougule-droid <samikshachougule2905@gmail.com>
2026-02-22 11:14:53 +0530

    Update App.py

--- App.py
+++ App.py
@@ -13,39 +13,39 @@ import joblib
 
 model = joblib.load("dtc_model.pkl")
 
-st.title("wine type prediction !")
+st.title("Wine Type Prediction")
 
-fixed_acidity	= st.number_input("fixed_acidity")
+fixed_acidity = st.number_input("fixed_acidity")
 volatile_acidity = st.number_input("volatile_acidity")
-citric_acid	= st.number_input("citric_acid")
+citric_acid = st.number_input("citric_acid")
 residual_sugar = st.number_input("residual_sugar")
-chlorides	= st.number_input("chlorides")
-free_sulfur_dioxide	= st.number_input("free_sulfur_dioxide")
+chlorides = st.number_input("chlorides")
+free_sulfur_dioxide = st.number_input("free_sulfur_dioxide")
 total_sulfur_dioxide = st.number_input("total_sulfur_dioxide")
 density = st.number_input("density")
 pH = st.number_input("pH")
-sulphates	= st.number_input("sulphates")
-alcohol	= st.number_input("alcohol")
-quality	= st.number_input("quality")
+sulphates = st.number_input("sulphates")
+alcohol = st.number_input("alcohol")
 
 input_data = pd.DataFrame({
-    "fixed_acidity" : [fixed_acidity],
-    "volatile_acidity" : [volatile_acidity],
-    "citric_acid" : [citric_acid],
-    "residual_sugar" : [residual_sugar],
-    "chlorides" : [chlorides],
-    "free_sulfur_dioxide" : [free_sulfur_dioxide],
-    "total_sulfur_dioxide" : [total_sulfur_dioxide],
-    "density" : [density],
-    "pH" : [pH],
-    "sulphates" : [sulphates],
-    "alcohol" : [alcohol],
-    "quality" : [quality]
+    "fixed_acidity": [fixed_acidity],
+    "volatile_acidity": [volatile_acidity],
+    "citric_acid": [citric_acid],
+    "residual_sugar": [residual_sugar],
+    "chlorides": [chlorides],
+    "free_sulfur_dioxide": [free_sulfur_dioxide],
+    "total_sulfur_dioxide": [total_sulfur_dioxide],
+    "density": [density],
+    "pH": [pH],
+    "sulphates": [sulphates],
+    "alcohol": [alcohol]
 })
 
-if st.button("predict"):
+# Prediction
+if st.button("Predict"):
     prediction = model.predict(input_data)[0]
+
     if prediction == "white":
-        st.success("white wine")
+        st.success("White Wine ")
     else:
-        st.error("red wine")
+        st.error("Red Wine ")
